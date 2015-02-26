773f63d516358b2f0e9f062374c6049500056e10
Anusree <anusree.a04@gmail.com>
2015-02-27 00:08:23 +0530

    Small changes

--- class_basic.py
+++ class_basic.py
@@ -1,8 +1,1 @@
-class point:
-	print "The class point"
-blank=point()
-print blank
-blank.x=3.0
-blank.y=4.0
-print blank.x
-print blank .y
+
--- file/file_basic.py
+++ file/file_basic.py
@@ -11,9 +11,25 @@ for x in f.readlines():
         print x
 f.close()
 
-#################################
+##################################
 #list(f) read all lines from the file in a list
 f=open('file1','r')
 print list(f)
 f.close()
+
+##################################
+#write(string) writes the content of string to the file
+f=open('file1','w')
+f.write('This is anusree\n')
+f.write('hello world\n')
+f.close()
+
+#################################
+#To write something other than string needs to be converted to string first
+f=open('file1','a')
+value=('The answer is: ', 42)
+s=str(value)
+f.write(s)
+f.close() 
+
 
