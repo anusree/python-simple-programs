678e49a8eb01e12f6220d710821398006550f444
Anusree <anusree.a04@gmail.com>
2015-03-17 17:34:51 +0530

    Moving_ball_without_keyboard_instruction_using_velocity

---
+++ an_itroduction_to_intractive_programming_in_python_part_1/moving_ball_without_keyinput.py
@@ -0,0 +1,33 @@
+import simplegui
+
+WIDTH = 600
+HEIGHT = 400
+BALL_RADIUS = 20
+
+ball_pos = [WIDTH / 2, HEIGHT / 2]
+vel = [0,1]
+#time = 0
+
+#def tick():
+#    global time
+#    time +=1
+
+def draw(canvas):
+    #ball_pos = [0, 0]
+    
+    #ball_pos[0] = init_pos[0] + time * vel[0]
+    #ball_pos[1] = init_pos[1] + time * vel[1]
+    ball_pos[0] += vel[0]
+    ball_pos[1] += vel[1]
+    
+    
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
+    
+
+frame = simplegui.create_frame("Positional ball control", WIDTH, HEIGHT)
+
+frame.set_draw_handler(draw)
+#timer = simplegui.create_timer(100, tick)
+
+#timer.start()
+frame.start()
